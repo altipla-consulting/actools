c766622daa7a84c212004f6d6b8e391c208692c9
Ernesto Alejo <ernestokarim@gmail.com>
2017-12-24 14:49:23 +0100

    Allow mapping of tools to containers to reuse images.

--- actools/main.go
+++ actools/main.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/altipla-consulting/collections"
 )
+
+var containers = []string{
+	"glide",
+	"go",
+	"node",
+}
+
+var toolContainers = map[string]string{
+	"npm": "node",
+}
 
 func main() {
 	if len(os.Args) == 1 {
@@ -12,20 +24,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	container := fmt.Sprintf("eu.gcr.io/altipla-tools/%s:latest", os.Args[1])
+	containerName, ok := toolContainers[os.Args[1]]
+	if !ok {
+		containerName = os.Args[1]
+	}
+	container := fmt.Sprintf("eu.gcr.io/altipla-tools/%s:latest", containerName)
+
 	root, err := os.Getwd()
 	if err != nil {
 		fmt.Println("cannot obtain current working directory:", err.Error())
 		os.Exit(3)
 	}
-	switch os.Args[1] {
-	case "glide", "go":
-		if err := runGeneric(root, container, os.Args[1:]); err != nil {
+
+	switch  {
+	case collections.HasString(containers, containerName):
+		sh := []string{
+			"run", "--rm",
+			"-it",
+			"--user", fmt.Sprintf("%d:%d", os.Getuid(), os.Getgid()),
+			"-v", fmt.Sprintf("%s:/staging", root),
+			container,
+		}
+		sh = append(sh, os.Args[1:]...)
+
+		if err := runCmd("docker", sh...); err != nil {
 			fmt.Println("error running tool:", err.Error())
 			os.Exit(2)
 		}
 
-	case "pull":
+	case os.Args[1] == "pull":
 		if err := pullContainers(); err != nil {
 			fmt.Println("error running pull:", err.Error())
 			os.Exit(2)	
@@ -53,29 +80,8 @@ func runCmd(name string, args ...string) error {
 	return nil
 }
 
-func runGeneric(root, container string, args []string) error {
-	sh := []string{
-		"run", "--rm",
-		"-it",
-		"--user", fmt.Sprintf("%d:%d", os.Getuid(), os.Getgid()),
-		"-v", fmt.Sprintf("%s:/staging", root),
-		container,
-	}
-	sh = append(sh, args...)
-
-	if err := runCmd("docker", sh...); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func pullContainers() error {
-	tools := []string{
-		"glide",
-		"go",
-	}
-	for _, tool := range tools {
+	for _, tool := range containers {
 		if err := runCmd("docker", "pull", fmt.Sprintf("eu.gcr.io/altipla-tools/%s:latest", tool)); err != nil {
 			return err
 		}
