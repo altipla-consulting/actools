ff905e746e4408845e0f2c50f20fde2a3a4278b2
Ernesto Alejo <ernestokarim@gmail.com>
2018-08-18 23:46:42 +0200

    dev-appengine: Escucha cambios en cualquier parte del GOPATH para recompilar. dev-appengine: Comparte un workdir común entre ejecuciones para acelerar la compilación.
    Change-Id: I539ee3e2c576c62691978714ad6faf413d9c7d9b

--- containers/dev-appengine/run.sh
+++ containers/dev-appengine/run.sh
@@ -2,4 +2,7 @@
 
 set -eu
 
-dev_appserver.py --datastore_path /opt/local/datastore --host 0.0.0.0 --admin_host 0.0.0.0 --port 8080 appengine
+mkdir -p /opt/local/workdir/default
+
+cd /go/src/$PROJECT/$WORKDIR/appengine
+dev_appserver.py . --datastore_path /opt/local/datastore --host 0.0.0.0 --admin_host 0.0.0.0 --port 8080 --go_work_dir /opt/local/workdir --enable_watching_go_path
