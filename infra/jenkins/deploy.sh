02c8ec3f4563b19304254e94de00b78cbb52ab61
Ernesto Alejo <ernestokarim@gmail.com>
2018-08-19 20:01:52 +0200

    infra: Elimina el Jenkinsfile y pasa a usar un script plano.
    Change-Id: I76ce1495ff66ef08126a80f7d5ef8eeb12a90981

---
+++ infra/jenkins/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+. /opt/ci-toolset/functions.sh
+
+GOOGLE_PROJECT=altipla-tools
+
+for FILE in containers/*/Dockerfile; do
+  APP=$(basename $(dirname $FILE))
+  docker-build-autotag eu.gcr.io/$GOOGLE_PROJECT/$APP containers/$APP/Dockerfile containers/$APP
+done
+
+run "sed -i 's/dev/${build-tag}/g' pkg/config/version.go"
+run 'actools go build -o actools ./cmd/actools'
+run "gsutil -h 'Cache-Control: no-cache' cp actools gs://tools.altipla.consulting/bin/actools"
+
+run "echo ${build-tag} > version"
+run "gsutil -h 'Cache-Control: no-cache' cp version gs://tools.altipla.consulting/version-manifest/actools"
+
