553258dda4ad40fecd95690fe66a8a23685a3c9f
Ernesto Alejo <ernestokarim@gmail.com>
2018-08-20 16:37:00 +0200

    gcloud: Configura la carpeta vendor para evitar el error de compilación.
    Change-Id: I6f1fd8b9c23599a1dafedd61b91a3ab04baf4d7b

--- containers/gcloud/deploy-appengine.sh
+++ containers/gcloud/deploy-appengine.sh
@@ -5,6 +5,9 @@ set -eu
 mkdir -p /go/src/$(dirname $PROJECT)
 ln -s /workspace /go/src/$PROJECT
 
-export GOPATH=/go
+mkdir -p /vendor
+ln -s /vendor/src /go/src/$PROJECT/vendor
+
+export GOPATH=/vendor:/go
 
 gcloud app deploy $*
