91f8eac56c91a1e04846e0170de1d5435aa1f18c
Ernesto Alejo <ernestokarim@gmail.com>
2018-10-13 23:56:27 +0200

    Update Go AppEngine to Go 1.11.
    Change-Id: If40cc2e9f35befeb826541e41a955c6807c56c1d

--- containers/dev-appengine/run.sh
+++ containers/dev-appengine/run.sh
@@ -2,7 +2,4 @@
 
 set -eu
 
-mkdir -p /opt/local/workdir/default
-
-cd /workspace/$WORKDIR/appengine
-dev_appserver.py . --datastore_path /opt/local/datastore --host 0.0.0.0 --admin_host 0.0.0.0 --port 8080 --go_work_dir /opt/local/workdir --enable_watching_go_path
+dev_appserver.py /workspace/$WORKDIR/app.yaml --datastore_path /home/container/datastore --host 0.0.0.0 --admin_host 0.0.0.0 --port 8080
