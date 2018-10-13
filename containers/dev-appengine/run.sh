b4613f50a92dcab2cc7d3b5306d935797c15f1a6
Ernesto Alejo <ernestokarim@gmail.com>
2018-10-13 23:09:10 +0200

    Fix paths to the shared Go code in App Engine.
    Change-Id: I1030893d00ef6e38ee43f6e6a60ab6880ad58aaf

--- containers/dev-appengine/run.sh
+++ containers/dev-appengine/run.sh
@@ -4,5 +4,5 @@ set -eu
 
 mkdir -p /opt/local/workdir/default
 
-cd /go/src/$PROJECT/$WORKDIR/appengine
+cd /workspace/$WORKDIR/appengine
 dev_appserver.py . --datastore_path /opt/local/datastore --host 0.0.0.0 --admin_host 0.0.0.0 --port 8080 --go_work_dir /opt/local/workdir --enable_watching_go_path
