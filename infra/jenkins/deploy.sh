a0c30531c6b4a4ae75a23177b614b261773ed470
Ernesto Alejo <ernestokarim@gmail.com>
2018-10-31 22:52:23 +0100

    Change the order of deployment to speed up changes in the binary.

--- infra/jenkins/deploy.sh
+++ infra/jenkins/deploy.sh
@@ -8,11 +8,6 @@ GOOGLE_PROJECT=altipla-tools
 
 configure-google-cloud
 
-for FILE in containers/*/Dockerfile; do
-  APP=$(basename $(dirname $FILE))
-  docker-build-autotag eu.gcr.io/$GOOGLE_PROJECT/$APP containers/$APP/Dockerfile containers/$APP
-done
-
 run "sed -i 's/dev/$(build-tag)/g' pkg/config/version.go"
 run 'actools go build -o actools ./cmd/actools'
 run "gsutil -h 'Cache-Control: no-cache' cp actools gs://tools.altipla.consulting/bin/actools"
@@ -21,5 +16,10 @@ run "echo $(build-tag) > version"
 run "gsutil -h 'Cache-Control: no-cache' cp version gs://tools.altipla.consulting/version-manifest/actools"
 
 run "gsutil -h 'Cache-Control: no-cache' cp install/install.sh gs://tools.altipla.consulting/install/actools"
+
+for FILE in containers/*/Dockerfile; do
+  APP=$(basename $(dirname $FILE))
+  docker-build-autotag eu.gcr.io/$GOOGLE_PROJECT/$APP containers/$APP/Dockerfile containers/$APP
+done
 
 git-tag
